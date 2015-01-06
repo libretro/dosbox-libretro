443c62c5a4e03f012be71f111090ea2dc3a4b35b
twinaphex <libretro@gmail.com>
2015-01-06 20:26:10 +0100

    (r3883) Add support for M_TANDY16 to GetPixel(). Fixes text and mouse pointer drawing in Tandy graphics modes.

--- src/ints/int10_put_pixel.cpp
+++ src/ints/int10_put_pixel.cpp
@@ -134,6 +134,26 @@ void INT10_PutPixel(Bit16u x,Bit16u y,Bit8u page,Bit8u color) {
 			// putpixel warining?
 			break;
 		}
+   case M_TANDY16:
+      {
+         bool is_32k = (real_readb(BIOSMEM_SEG, BIOSMEM_CURRENT_MODE) >= 9)?true:false;
+         Bit16u segment, offset;
+         if (is_32k) {
+            if (machine==MCH_PCJR) {
+               Bitu cpupage = (real_readb(BIOSMEM_SEG, BIOSMEM_CRTCPU_PAGE) >> 3) & 0x7;
+               segment = cpupage << 10;
+            } else segment = 0xb800;
+            offset = (y >> 2) * (CurMode->swidth >> 1) + (x>>1);
+            offset += (8*1024) * (y & 3);
+         } else {
+            segment = 0xb800;
+            offset = (y >> 1) * (CurMode->swidth >> 1) + (x>>1);
+            offset += (8*1024) * (y & 1);
+         }
+         Bit8u val=real_readb(segment,offset);
+         *color=(val>>((x&1)?0:4)) & 0xf;
+      }
+      break;
 	case M_EGA:
 		{
 			/* Set the correct bitmask for the pixel position */
