e9c77eee956f5c260b363e3ea889af8d756f871f
Andrés <fr500@users.noreply.github.com>
2016-01-03 14:30:04 -0500

    3DS build fix

--- include/config.h
+++ include/config.h
@@ -66,7 +66,7 @@
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_UNISTD_H 1
 
-#if !defined(__WIN32__) && !defined(__POWERPC__) && !defined(VITA)
+#if !defined(__WIN32__) && !defined(__POWERPC__) && !defined(VITA) && !defined(_3DS)
 # define HAVE_PWD_H 1
 #endif
 
